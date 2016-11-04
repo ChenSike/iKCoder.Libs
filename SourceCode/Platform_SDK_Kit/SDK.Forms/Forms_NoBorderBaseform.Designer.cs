33130d670d44f002a5b736092f6ecf39b74db454
Sike.Chen <sike.chen@outlook.com>
2016-11-04 20:56:39 +0800

    new code for form
    new code

--- SourceCode/Platform_SDK_Kit/SDK.Forms/Forms_NoBorderBaseform.Designer.cs
+++ SourceCode/Platform_SDK_Kit/SDK.Forms/Forms_NoBorderBaseform.Designer.cs
@@ -33,9 +33,8 @@
             // Forms_NoBorderBaseform
             // 
             this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(284, 242);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Forms_NoBorderBaseform";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
